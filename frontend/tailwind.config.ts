f267cd2b51e63837cef875a19ac898627df83bfc
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-06-22 00:12:40 -0300

    feat: add css classes

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -12,17 +12,22 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      },   
       colors: {
-        primary: 'rgba(25,30,36,1)',
-        secondary: '#2a323c'
+        primary: "rgba(25, 30, 36, 1)",
+        secondary: "#2A323C",
+        bar: "#1D232A",
+        "btn-primary": "#7480FF",
+        input: "#1d232a",
       },
       textColor: {
-        default: '#a6adbb'
+        default: "#a6adbb",
+        "btn-primary": "#050617",
+        subtitle: "#7480FF",
       },
       gridTemplateColumns: {
-        "auto-fit-cards": "repeat(auto-fit, minmax(277px, 1fr))"
-      }
+        "auto-fit-cards": "repeat(auto-fit, minmax(277px, 1fr))",
+      },
     },
   },
   plugins: [],
