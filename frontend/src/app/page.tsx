5f1c68d355a002046ccb8700cd449315a77a19c0
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-06-21 23:09:13 -0300

    feat: event card

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,45 @@
+import EventCard from "@/components/EventCard";
+import Title from "@/components/Title";
+import { EventModel } from "@/models";
 import Image from "next/image";
 
 export default function Home() {
+  const events: EventModel[] = [
+    {
+      id: "1",
+      name: "Desenvolvimento de software",
+      date: "2022-12-31T00:00:00.000Z",
+      organization: "Cubos",
+      location: "São Paulo",
+    },
+    {
+      id: "1",
+      name: "Desenvolvimento de software",
+      date: "2022-12-31T00:00:00.000Z",
+      organization: "Cubos",
+      location: "São Paulo",
+    },
+    {
+      id: "1",
+      name: "Desenvolvimento de software",
+      date: "2022-12-31T00:00:00.000Z",
+      organization: "Cubos",
+      location: "São Paulo",
+    },
+    {
+      id: "1",
+      name: "Desenvolvimento de software",
+      date: "2022-12-31T00:00:00.000Z",
+      organization: "Cubos",
+      location: "São Paulo",
+    },
+  ]
   return (
-   <></>
+    <main className="mt-10 flex flex-col">
+      <Title>Eventos disponíveis</Title>
+      <div className="mt-8 sm:grid sm:grid-cols-auto-fit-cards flex flex-wrap justify-center gap-x-2 gap-y-4">
+        {events.map((event) => <EventCard key={event.id} event={event}></EventCard>)}
+      </div>
+    </main>
   )
 }
